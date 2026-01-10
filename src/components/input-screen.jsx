739a0c1d9dec460cc651fc2967c7b282d441fc95
JayendraLal Joshi <jayendralal.joshi@nielseniq.com>
2026-01-10 19:41:20 +0100

    Add components and structure directory

---
+++ src/components/input-screen.jsx
@@ -0,0 +1,12 @@
+
+function Heading({text}){
+    return(
+        <h1 className="heading">{text}</h1>
+    )
+}
+
+export function InputScreen(){
+    return(
+        <Heading text = "CV Builder"/>
+    )
+}
