b92a32f9bab2801af6635b18c7755b98877d0bd6
Jayendra Lal Joshi <Jayendralal.joshi@nielseniq.com>
2026-01-19 18:32:28 +0100

    Style entries, refactor css file and use variables, add minimal hover styles

--- src/components/input-screen-components/entry.jsx
+++ src/components/input-screen-components/entry.jsx
@@ -1,5 +1,10 @@
+import editIcon from "../../assets/edit.svg";
+
 export function Entry({entryDetails,setOpenEntry, setShowForm}){
     return(
-        <button id={entryDetails.id} onClick={()=>{setOpenEntry([true,entryDetails.id]);setShowForm(true)}}>{Object.values(entryDetails)[0]}</button>
+        <button className="entry" id={entryDetails.id} onClick={()=>{setOpenEntry([true,entryDetails.id]);setShowForm(true)}}>
+            <h3>{Object.values(entryDetails)[0]}</h3>
+            <img src={editIcon} className="edit-icon"></img>
+            </button>
     )
 }
